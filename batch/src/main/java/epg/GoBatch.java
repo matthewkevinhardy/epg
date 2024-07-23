efb5a2dea0ae3a7fb1d81ac0d2027a65aa48e1b5
hardym <matthew.hardy@ynap.com>
2024-07-23 16:05:19 +0100

    batch updates

--- batch/src/main/java/epg/GoBatch.java
+++ batch/src/main/java/epg/GoBatch.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,13 +13,11 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class GoBatch implements CommandLineRunner {
 
-	private final JobLauncher jobLauncher;
-	private final ApplicationContext applicationContext;
+	@Autowired
+	private JobLauncher jobLauncher;
 
-	public GoBatch(JobLauncher jobLauncher, ApplicationContext applicationContext) {
-		this.jobLauncher = jobLauncher;
-		this.applicationContext = applicationContext;
-	}
+	@Autowired
+	private ApplicationContext applicationContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoBatch.class, args).close();
