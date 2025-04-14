8dd86205a0fe4517ab4019784e7805321dc48919
Matt <Matt@Maximus>
2025-04-14 23:27:56 +0100

    ZoneOffset.UTC

--- search/src/main/java/epg/search/cache/NowExpiryPolicy.java
+++ search/src/main/java/epg/search/cache/NowExpiryPolicy.java
@@ -1,6 +1,7 @@
 package epg.search.cache;
 
 import java.time.Duration;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.function.Supplier;
 
@@ -12,17 +13,17 @@ public class NowExpiryPolicy implements ExpiryPolicy<String, ProgrammeDoc> {
 
 	@Override
 	public Duration getExpiryForCreation(String key, ProgrammeDoc value) {
-		return Duration.between(ZonedDateTime.now(), value.getStop());
+		return Duration.between(ZonedDateTime.now(ZoneOffset.UTC), value.getStop());
 	}
 
 	@Override
 	public Duration getExpiryForAccess(String key, Supplier<? extends ProgrammeDoc> value) {
-		return Duration.between(ZonedDateTime.now(), value.get().getStop());
+		return Duration.between(ZonedDateTime.now(ZoneOffset.UTC), value.get().getStop());
 	}
 
 	@Override
 	public Duration getExpiryForUpdate(String key, Supplier<? extends ProgrammeDoc> oldValue, ProgrammeDoc newValue) {
-		return Duration.between(ZonedDateTime.now(), newValue.getStop());
+		return Duration.between(ZonedDateTime.now(ZoneOffset.UTC), newValue.getStop());
 	}
 
 }
